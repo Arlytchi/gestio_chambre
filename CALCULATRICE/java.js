bed2a9411dfa28758bcbe3697721bf465c7364b0
Arlytchi <118062500+Arlytchi@users.noreply.github.com>
2022-12-01 14:10:29 +0000

    cal

--- CALCULATRICE/java.js
+++ CALCULATRICE/java.js
@@ -1,13 +1,43 @@
+function Calcul() {
+    var x, y, addition;
+    
+    x = Number(document.getElementById("nbr1").value);
+    y = Number(document.getElementById("nbr2").value);
+    addition = x + y;
+    multiplication = x * y;
+    //Tester si le diviseur n'est pas égal à 0 avec if
+    division = x / y;
+    modulo = x % y;
 
-function calcul(){
-    var nbr1,nbr2,multi,addi,div;
-    multi = nbr1*nbr2;
-    addi = nbr1+nbr2;
-    div = nbr1/nbr2;
+    document.getElementById("addition").value = addition;
+    document.getElementById("multiplication").value = multiplication;
+    document.getElementById("division").value = division;
+    document.getElementById("modulo").value = modulo ;
 
-    nbr1=Number(document.getElementById("nbr1").value);
-    nbr2=Number(document.getElementById("nbr2").value);
-    document.getElementById("multi").value=multi;
-    document.getElementById("addi").value=addi;
-    document.getElementById("div").value=div;
+}
+
+function ControleSaisie(){
+    var nombre=document.getElementById("nbr1").value;
+    var nombre2=document.getElementById("nbr2").value; 
+    var Error="";
+    document.getElementById("erreur").innerHTML="";
+    if(nombre==""){
+        Error +="Veuillez renseigner le champ Nombre 1 <br>";
+    }
+
+    if(nombre2==""){
+        Error +="Veuillez renseigner le champ Nombre 2 <br>";
+    }
+    if(nombre2<="0" && nombre2!=""){
+        Error += "ERROR : DIVISION IMPOSSIBLE PAR 0"
+    }
+    
+    if(Error==""){
+        document.getElementById("valide").removeAttribute("disabled");
+    }
+    else{
+        document.getElementById("erreur").innerHTML=Error;
+        document.getElementById("valide").setAttribute("disabled", "true");
+
+    }
 }
